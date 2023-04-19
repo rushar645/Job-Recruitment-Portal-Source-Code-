a25471d815cf12627fa6b5be137927b78df23a8b
Purushartha Singh Thakur <purushartha3011@gmail.com>
2023-04-19 22:53:08 +0530

    Update init.js

--- backend/database/init.js
+++ backend/database/init.js
@@ -1,5 +1,7 @@
 const mongoose=require("mongoose");
-
+const dotenv = require('dotenv');
+dotenv.config();
+const uri = process.env.mongodbURI;
 module.exports=function(){
   return mongoose.connect(uri);
 }
